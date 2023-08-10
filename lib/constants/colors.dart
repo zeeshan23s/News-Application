cda009bbd230d4cf325523ea7533add3da427bb1
Muhammad Zeeshan Tassawar <codechaser209@gmail.com>
2023-08-10 17:18:27 +0500

    Startup Code

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import '../packages_export.dart';
+
+const Color primaryColor = Color(0xFFFEFEFE);
+const Color secondaryColor = Color(0xFF181818);
+const Color progressColor = Color(0xFF9D9BFF);
+const Color reviewColor = Color(0xFFEDA7FF);
+const Color holdColor = Color(0xFFF6BB54);
+const Color completedColor = Color(0xFF4DBF59);
+const Color unselectedColor = Color(0xFFDCDCDC);
+
+const Color lowIntensity = Color(0xFF4DBF59);
+const Color mediumIntensity = Color(0xFFF6BB54);
+const Color highIntensity = Color(0xFFFDADAD);
---
+++ lib/packages_export.dart
@@ -0,0 +1,11 @@
+// Design Exports
+export 'package:flutter/material.dart';
+export 'package:google_fonts/google_fonts.dart';
+
+// Project Folders Exports
+export 'screens/screens.dart';
+export 'constants/constant.dart';
+export 'utils/util.dart';
+export 'services/service.dart';
+export 'widgets/widgets.dart';
+export 'models/model.dart';
