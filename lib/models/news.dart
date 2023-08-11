9e5ded7c935c86450fc22e4c647b0542f4e98501
Muhammad Zeeshan Tassawar <codechaser209@gmail.com>
2023-08-11 16:33:49 +0500

    New Model Updated

--- lib/models/news.dart
+++ lib/models/news.dart
@@ -1,28 +1,99 @@
+import 'dart:convert';
+
 class News {
-  late final String source;
-  late final String? author;
-  late final String title;
-  late final String description;
-  late final String url;
-  late final String? urlToImage;
-  late final String publishedAt;
+  final String source;
+  final String? author;
+  final String title;
+  final String description;
+  final String url;
+  final String? urlToImage;
+  final String publishedAt;
 
-  News(
-      {required this.source,
-      this.author,
-      required this.title,
-      required this.description,
-      required this.url,
-      this.urlToImage,
-      required this.publishedAt});
+  News({
+    required this.source,
+    this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    this.urlToImage,
+    required this.publishedAt,
+  });
 
-  News.fromJson(Map<String, dynamic> json) {
-    source = json['source']['name'];
-    author = json['author'];
-    title = json['title'];
-    description = json['description'];
-    url = json['url'];
-    urlToImage = json['urlToImage'];
-    publishedAt = json['publishedAt'];
+  News copyWith({
+    String? source,
+    String? author,
+    String? title,
+    String? description,
+    String? url,
+    String? urlToImage,
+    String? publishedAt,
+  }) {
+    return News(
+      source: source ?? this.source,
+      author: author ?? this.author,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      url: url ?? this.url,
+      urlToImage: urlToImage ?? this.urlToImage,
+      publishedAt: publishedAt ?? this.publishedAt,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'source': source,
+      'author': author,
+      'title': title,
+      'description': description,
+      'url': url,
+      'urlToImage': urlToImage,
+      'publishedAt': publishedAt,
+    };
+  }
+
+  factory News.fromMap(Map<String, dynamic> map) {
+    return News(
+      source: map['source']['name'],
+      author: map['author'],
+      title: map['title'],
+      description: map['description'],
+      url: map['url'],
+      urlToImage: map['urlToImage'],
+      publishedAt: map['publishedAt'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory News.fromJson(Map<String, dynamic> source) => News.fromMap(source);
+
+  @override
+  String toString() {
+    return 'News(source: $source, author: $author, title: $title, description: $description, url: $url, urlToImage: $urlToImage, publishedAt: $publishedAt)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is News &&
+        other.source == source &&
+        other.author == author &&
+        other.title == title &&
+        other.description == description &&
+        other.url == url &&
+        other.urlToImage == urlToImage &&
+        other.publishedAt == publishedAt;
+  }
+
+  @override
+  int get hashCode {
+    return source.hashCode ^
+        author.hashCode ^
+        title.hashCode ^
+        description.hashCode ^
+        url.hashCode ^
+        urlToImage.hashCode ^
+        publishedAt.hashCode;
   }
 }
