ad9df330246b04b44a42ee04ff064c82feb2f2c9
Muhammad Zeeshan Tassawar <codechaser209@gmail.com>
2023-08-21 08:37:13 +0500

    Query Parameters Updated

--- lib/services/news_controller.dart
+++ lib/services/news_controller.dart
@@ -5,12 +5,12 @@ class NewsController {
     Dio dio = Dio();
     List<News> appNews = [];
 
-    final response =
-        await dio.get('https://newsapi.org/v2/everything', queryParameters: {
-      'q': relatedTo.toLowerCase(),
-      'apiKey': '4fb918f8585943ecb9e32ab3c4f3167b',
-      'language': 'en'
-    });
+    final response = await dio.get('https://newsapi.org/v2/everything',
+        queryParameters: {
+          'q': relatedTo.toLowerCase(),
+          'apiKey': 'APIKEY',
+          'language': 'en'
+        });
 
     if (response.statusCode == 200) {
       List<dynamic> data = response.data['articles'];
